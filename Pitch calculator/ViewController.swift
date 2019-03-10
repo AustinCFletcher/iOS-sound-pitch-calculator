666c4a13898fbbb22986c0876617a6e6acc8cf1d
Austin Fletcher <afletcher@firstorion.com>
2019-03-10 14:39:55 -0500

    Initial Commit

---
+++ Pitch calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pitch calculator
+//
+//  Created by Austin Fletcher on 3/10/19.
+//  Copyright © 2019 Austin Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
