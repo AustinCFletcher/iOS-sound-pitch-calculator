f95ffe74f59defa53c2fd9a2d69fb09a63a3eea7
Austin Fletcher <afletcher@firstorion.com>
2019-03-24 14:08:48 -0500

    fleshed out the functional osc and signal factory

--- Pitch calculator/Sound/FPOscillator.swift
+++ Pitch calculator/Sound/FPOscillator.swift
@@ -13,7 +13,13 @@ public typealias Signal = (Int) -> Float
 class SignalFactory {
     
     public func build(waveType: OscillatorWaveType, freq: Float) -> Signal {
-        return sineWave(frequency: freq)
+        
+        switch(waveType) {
+        case .sine: return sineWave(frequency: freq)
+        case .square: return squareWave(frequency: freq)
+        case .triangle: return triangleWave(frequency: freq)
+        case .saw: return sawWave(frequency: freq)
+        }
     }
     
     private func sineWave(frequency: Float) -> Signal {
@@ -23,11 +29,38 @@ class SignalFactory {
         }
     }
     
+    private func sawWave(frequency: Float) -> Signal {
+        let phi = frequency / Sound.sampleRate
+        return { i in
+            let phase = (Float(i) * phi).truncatingRemainder(dividingBy: Sound.twoPI)
+            return 1.0 - (2.0 * phase )
+        }
+    }
+    
+    private func squareWave(frequency: Float) -> Signal {
+        let phi = frequency / Sound.sampleRate
+        return { i in
+            let phase = (Float(i) * phi).truncatingRemainder(dividingBy: Sound.twoPI)
+            return (phase <= Sound.PI) ? 1.0 : -1.0
+        }
+    }
+    
+    private func triangleWave(frequency: Float) -> Signal {
+        let phi = frequency / Sound.sampleRate
+        return { i in
+            let phase = (Float(i) * phi).truncatingRemainder(dividingBy: Sound.twoPI)
+            var value = -1.0 + (2.0 * phase / Sound.twoPI);
+            value = 2.0 * (abs(value) - 0.5)
+            return value
+        }
+    }
 }
 
 // A more functional-programming approach to oscillator, wrapper of a Signal essentially
 
 class FPOscillator {
+    
+    // MARK: - Properties
     
     private var oscillatorWaveType: OscillatorWaveType = .sine {
         didSet { updateSignalFunc(frequency, oscillatorWaveType: oscillatorWaveType) }
@@ -36,12 +69,29 @@ class FPOscillator {
         didSet { updateSignalFunc(frequency, oscillatorWaveType: oscillatorWaveType) }
     }
     private var signal: Signal
+    private var amplitude: Float = 1
     
     public init(frequency: Float = 440.0,
                 oscillatorWaveType: OscillatorWaveType = .sine) {
         self.frequency = frequency
         self.oscillatorWaveType = oscillatorWaveType
         self.signal = SignalFactory().build(waveType: oscillatorWaveType, freq: frequency)
+    }
+    
+    public func setAmplitude(_ amplitude: Float) {
+        self.amplitude = amplitude
+    }
+    
+    public func setFrequency(_ frequency: Float) {
+        self.frequency = frequency
+    }
+    
+    public func setWaveType(_ oscillatorWaveType: OscillatorWaveType) {
+        self.oscillatorWaveType = oscillatorWaveType
+    }
+    
+    public func sample(_ index: Int) -> Float {
+        return signal(index) * amplitude
     }
     
     private func updateSignalFunc(_ frequency: Float, oscillatorWaveType: OscillatorWaveType) {
